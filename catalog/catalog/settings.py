d510b31206fccb3036bba7d6d35d21cfe8a751fd
Myroslav Kolesnykov <miroslavkolesnykov@gmail.com>
2025-04-14 10:10:18 +0300

    settings

--- catalog/catalog/settings.py
+++ catalog/catalog/settings.py
@@ -127,7 +127,7 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 # EMAIL_HOST = "smtp.gmail.com"
 # EMAIL_PORT = 587
 # EMAIL_HOST_USER = "myroslavkolesnykov@gmail.com"
-# EMAIL_HOST_PASSWORD = "MrPrince14"
+# EMAIL_HOST_PASSWORD = ""
 # EMAIL_USE_TLS = True
 # DEFAULT_FROM_EMAIL = "My shop myroslavkolesnykov@gmail.com"
 
